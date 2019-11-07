348ef8d47dcb55d931247f91787a3df49b40ae0f
BobinShihJosh <49407349+BobinShihJosh@users.noreply.github.com>
2019-11-06 21:39:45 -0800

    Update Single_Cycle_CPU.sv

--- Lab3/Single_Cycle_CPU.sv
+++ Lab3/Single_Cycle_CPU.sv
@@ -9,7 +9,7 @@ module Single_Cycle_CPU(reset,clk);
 	output logic  zero,negative,overflow,carry_out;
 	
 	
-	logic [63:0]dataA,dataB,WriteData,ALUOut,PC,PCnew;
+	logic [63:0]dataA,dataB,WriteData,ALUOut,PC,PCnew, PC4;
 	logic [31:0] instr;    //instruction code used for the CPU
 	logic [4:0] OPID;			//instruction ID decoded from control signal 
 	logic WrEn,ALUOp,MemWrite,read_enable;
@@ -24,7 +24,8 @@ module Single_Cycle_CPU(reset,clk);
 	
 	REG PCreg(PCnew, PC , clk, '1);
 	
-	PCpath (OPID, instr,PCnew);
+	//PC4 is the wire out signal, use it for BR instr
+	PCpath (OPID, PC, unCondBr, BrTaken, Db, Imm19, Imm26, PCNew, PC4);
 	
 	datapath (clk,Reg2Loc,ALUsrc,MemToReg, RegWrite,MemWrite, ConstSel,Reg3Loc,ALUOP,Imm9,Imm12,Imm19,Imm26,Rn,Rd,Rm,flags);
 
@@ -40,4 +41,4 @@ module Single_Cycle_CPU(reset,clk);
 	
 	
 	datamem MEM(ALUOut,MemWrite,read_enable,write_data,clk,xfer_size,read_data	);*/
-
+endmodule
